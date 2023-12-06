2c1ccf6e4bccba4b3ac87b8deacba756dc2b171f
Dr. Chalece Cume <68288735+DrChaleceCume@users.noreply.github.com>
2023-12-06 03:23:25 +0300

    how are these even there still

--- files/inv_controller.lua
+++
@@ -1,8 +0,0 @@
-dofile_once( "mods/index_core/files/_lib.lua" )
-
-local controller_id = GetUpdatedEntityID()
-
---continously disable itempickupper and inventorygui
---check controls comp to set enabled or not (forced_state -1 is always disabled and 1 is always enabled)
-
---bars
